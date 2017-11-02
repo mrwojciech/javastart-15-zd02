3ed3e38de1d8619688f5aeb032a91e8770760fcc
Wojciech Sobasik <wojciech.sobasik@gmail.com>
2017-11-02 16:16:24 +0100

    zd2 init

---
+++ src/main/java/com/example/zd1502/Zd1502Application.java
@@ -0,0 +1,12 @@
+package com.example.zd1502;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zd1502Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zd1502Application.class, args);
+	}
+}
