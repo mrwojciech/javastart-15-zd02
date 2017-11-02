bb29c803aa6244f5d92bc01b5c8cf53f4b69129b
Wojciech Sobasik <wojciech.sobasik@gmail.com>
2017-11-02 16:16:13 +0100

    zd2 init

---
+++ src/test/java/com/example/zd1502/Zd1502ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.zd1502;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Zd1502ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
